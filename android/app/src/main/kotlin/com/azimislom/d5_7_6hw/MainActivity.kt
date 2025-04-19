bd2fa30f46503a3fa31d596099c583ce7dfde083
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-19 17:44:42 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/azimislom/d5_7_6hw/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.azimislom.d5_7_6hw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:d5_7_6hw/pages/home_page.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      builder: (context) => MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
